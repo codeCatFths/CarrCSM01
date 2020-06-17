aa15b7d12fdb3d3c0b15c8f5f841015a14ecdc60
codeCatFths <54652836+codeCatFths@users.noreply.github.com>
2020-06-17 10:33:49 -0700

    Update behavior2.js

--- CarrCowkowlator/behavior2.js
+++ CarrCowkowlator/behavior2.js
@@ -48,7 +48,7 @@ function getRandom()
 // Responds to press of number keys, accepts numeric value
 function getDigit(digit)
 {
-    if(inputCounter == 0)
+    if(inputCounter == 0) // input of first operand
     {
         if(digit == 3.14159)
         {
@@ -59,11 +59,11 @@ function getDigit(digit)
         inputCounter = inputCounter + 1;
         document.getElementById("output").innerHTML = op1;
     }
-    else if(inputCounter == 1)
+    else if(inputCounter == 1) // error message - requires symbol input
     {
         document.getElementById("output").innerHTML = "+ - * /";
     }
-    else if(inputCounter == 2)
+    else if(inputCounter == 2) // input of second operand
     {
         if(digit == 3.14159)
         {
@@ -74,7 +74,7 @@ function getDigit(digit)
         inputCounter = inputCounter + 1;
         document.getElementById("output").innerHTML = op2;
     }
-    else
+    else // handles any key press other than "equals"
     {
         calculate();
     }
@@ -83,13 +83,13 @@ function getDigit(digit)
 // Responds to press of +,-,*,/ keys, accepts a string
 function getOperator(op)
 {
-    if(inputCounter == 1)
+    if(inputCounter == 1) // input of operator
     {
         operator = op;
         inputCounter = inputCounter + 1;
         document.getElementById("output").innerHTML = op;
     }
-    else
+    else // error message - requires digit input
     {
         if(inputCounter == 0 || inputCounter == 2)
             document.getElementById("output").innerHTML = "pick #";
@@ -111,64 +111,40 @@ function resetVariables()
     op2          = 0.0;
     operator     = "@";
     result       = 0.0; // calculation answer
-    inputCounter = 0; // track entries for calculation
+    inputCounter = 0;   // track entries for calculation
 } // end resetVariables()
 
-// Resonds to press of "equals" key, completes calculation, displays answer
+// Responds to press of "equals" key, completes calculation, displays answer
 function calculate()
 {
-    // performs calculation following entry of 2nd operand
-    if(inputCounter == 3)
+    
+    if(inputCounter == 3) // performs calculation following entry of 2nd operand
     {
         if(operator == "+") // addition
         {
             result = op1 + op2;
-            document.getElementById("output").innerHTML = "= " + result;
-            
-            // cele-moo-shun
-            moo.play();
         }
         else if(operator == "-") // subtraction
         {
             result = op1 - op2;
-            document.getElementById("output").innerHTML = "= " + result;
-            
-            // cele-moo-shun
-            moo.play();
         }
         else if(operator == "*") // multiplication
         {
             result = op1 * op2;
-            
-            if( op1 != 3.14159 && op2 != 3.14159 )
-            {
-                document.getElementById("output").innerHTML = "= " + result;
-            }
-            else // decreases decimal output to fit in display
-            {
-                document.getElementById("output").innerHTML = "= " + result.toFixed(5);
-            }
-            
-            // cele-moo-shun
-            moo.play();
         }
         else if(operator == "/" && op2 != 0) // division
         {
             result = op1 / op2;
-            
-            // decreases decimal output to fit in display
-            document.getElementById("output").innerHTML = "= " + result.toFixed(4);
-            
-            // cele-moo-shun
-            moo.play();
         }
         else // outputs division by zero error to display
         {
             // CREDIT: http://www.rosswalker.co.uk/movie_sounds/2001_and_2010.htm
-            var audio = new Audio('http://www.rosswalker.co.uk/movie_sounds/sounds_files_20150201_1096714/2001_and_2010/cantdo.wav');
-            audio.type = 'audio/wav';
-            audio.play();
+            var errorAudio = new Audio('http://www.rosswalker.co.uk/movie_sounds/sounds_files_20150201_1096714/2001_and_2010/cantdo.wav');
+            errorAudio.type = 'audio/wav';
+            errorAudio.play();
             document.getElementById("output").innerHTML = "No / by 0";
+            
+            break;
         }
     }
     else // handles inappropriate user entry
@@ -189,7 +165,21 @@ function calculate()
         {
             document.getElementById("output").innerHTML = "do over";
         }
+        
+        break;
     }
+    
+    if( op1 != 3.14159 && op2 != 3.14159 )
+    {
+        document.getElementById("output").innerHTML = "= " + result;
+    }
+    else // decreases decimal output to fit in display
+    {
+        document.getElementById("output").innerHTML = "= " + result.toFixed(5); // HELP: does not work on x - PI ???
+    }
+    
+    // cele-moo-shun
+    moo.play();
     
     // clear input for next round
     resetVariables();
