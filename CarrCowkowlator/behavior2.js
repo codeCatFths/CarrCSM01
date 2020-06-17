85f6253e90b8ae3c248da9736409e1dbce784e9d
codeCatFths <54652836+codeCatFths@users.noreply.github.com>
2020-06-17 14:57:58 -0700

    Update behavior2.js

--- CarrCowkowlator/behavior2.js
+++ CarrCowkowlator/behavior2.js
@@ -15,9 +15,9 @@ var op1          = 0.0;
 var op2          = 0.0;
 var operator     = "@";
 var result       = 0.0; // send to display
-var inputCounter = 0; // track entries for simple calculation process
+var inputCounter = 0;   // track entries for simple calculation process
 
-// Responds to press of "random" key, generates 1-100
+// responds to press of "random" key, generates 1-100
 function getRandom()
 {
     if(inputCounter == 0)
@@ -25,6 +25,7 @@ function getRandom()
         random = Math.floor(Math.random() * 100) + 1;
         op1 = random;
         inputCounter = inputCounter + 1;
+        
         document.getElementById("output").innerHTML = op1;
     }
     else if(inputCounter == 1)
@@ -36,19 +37,18 @@ function getRandom()
         random = Math.floor(Math.random() * 100) + 1;
         op2 = random;
         inputCounter = inputCounter + 1;
+        
         document.getElementById("output").innerHTML = op2;
     }
     else
     {
         calculate();
     }
-
 } // end getRandom()
 
-// Responds to press of number keys, accepts numeric value
 function getDigit(digit)
 {
-    if(inputCounter == 0) // input of first operand
+    if(inputCounter == 0)
     {
         if(digit == 3.14159)
         {
@@ -57,13 +57,14 @@ function getDigit(digit)
         
         op1 = digit;
         inputCounter = inputCounter + 1;
+        
         document.getElementById("output").innerHTML = op1;
     }
-    else if(inputCounter == 1) // error message - requires symbol input
+    else if(inputCounter == 1)
     {
         document.getElementById("output").innerHTML = "+ - * /";
     }
-    else if(inputCounter == 2) // input of second operand
+    else if(inputCounter == 2)
     {
         if(digit == 3.14159)
         {
@@ -72,38 +73,34 @@ function getDigit(digit)
         
         op2 = digit;
         inputCounter = inputCounter + 1;
+        
         document.getElementById("output").innerHTML = op2;
     }
-    else // handles any key press other than "equals"
+    else
     {
         calculate();
     }
-} // end getDigit(digit)
+}
 
-// Responds to press of +,-,*,/ keys, accepts a string
 function getOperator(op)
 {
-    if(inputCounter == 1) // input of operator
+    if(inputCounter == 1)
     {
         operator = op;
         inputCounter = inputCounter + 1;
+        
         document.getElementById("output").innerHTML = op;
     }
-    else // error message - requires digit input
+    else
     {
         if(inputCounter == 0 || inputCounter == 2)
+        {
             document.getElementById("output").innerHTML = "pick #";
+        }
     }
-} // end getOperator(op)
+}
 
-// Responds to press of C key, resets variables, reloads page
-function clearAll()
-{
-    resetVariables();
-    location.reload();
-} // end clearAll()
-
-// Resets variables to default values
+// resets variables to default values
 function resetVariables()
 {
     random       = 0.0;
@@ -112,39 +109,72 @@ function resetVariables()
     operator     = "@";
     result       = 0.0; // calculation answer
     inputCounter = 0;   // track entries for calculation
+    
 } // end resetVariables()
 
-// Responds to press of "equals" key, completes calculation, displays answer
+// responds to press of C key, resets variables, reloads page
+function clearAll()
+{
+    resetVariables();
+    location.reload();
+    
+} // end clearAll()
+
+// responds to press of "equals" key, completes calculation, displays answer
 function calculate()
 {
-    
-    if(inputCounter == 3) // performs calculation following entry of 2nd operand
+    // performs calculation following entry of 2nd operand
+    if(inputCounter == 3)
     {
         if(operator == "+") // addition
         {
             result = op1 + op2;
+            document.getElementById("output").innerHTML = "= " + result;
+            
+            // cele-moo-shun
+            moo.play();
         }
         else if(operator == "-") // subtraction
         {
             result = op1 - op2;
+            document.getElementById("output").innerHTML = "= " + result.toPrecision(5);
+            
+            // cele-moo-shun
+            moo.play();
         }
         else if(operator == "*") // multiplication
         {
             result = op1 * op2;
+            
+            if( op1 != 3.14159 && op2 != 3.14159 )
+            {
+                document.getElementById("output").innerHTML = "= " + result;
+            }
+            else // decreases decimal output to fit in display
+            {
+                document.getElementById("output").innerHTML = "= " + result.toFixed(5);
+            }
+            
+            // cele-moo-shun
+            moo.play();
         }
         else if(operator == "/" && op2 != 0) // division
         {
             result = op1 / op2;
+            
+            // decreases decimal output to fit in display
+            document.getElementById("output").innerHTML = "= " + result.toFixed(5);
+            
+            // cele-moo-shun
+            moo.play();
         }
         else // outputs division by zero error to display
         {
             // CREDIT: http://www.rosswalker.co.uk/movie_sounds/2001_and_2010.htm
-            var errorAudio = new Audio('http://www.rosswalker.co.uk/movie_sounds/sounds_files_20150201_1096714/2001_and_2010/cantdo.wav');
-            errorAudio.type = 'audio/wav';
-            errorAudio.play();
+            var audio = new Audio('http://www.rosswalker.co.uk/movie_sounds/sounds_files_20150201_1096714/2001_and_2010/cantdo.wav');
+            audio.type = 'audio/wav';
+            audio.play();
             document.getElementById("output").innerHTML = "No / by 0";
-            
-            break;
         }
     }
     else // handles inappropriate user entry
@@ -153,6 +183,7 @@ function calculate()
             document.getElementById("output").innerHTML = "pick #";
         
         if( inputCounter == 1 ) // ends process, show current op1 value
+            document.getElementById("output").innerHTML = "=" + op1;
         {
             if( op1 == 3.14159 )
             {
@@ -165,21 +196,7 @@ function calculate()
         {
             document.getElementById("output").innerHTML = "do over";
         }
-        
-        break;
     }
-    
-    if( op1 != 3.14159 && op2 != 3.14159 )
-    {
-        document.getElementById("output").innerHTML = "= " + result;
-    }
-    else // decreases decimal output to fit in display
-    {
-        document.getElementById("output").innerHTML = "= " + result.toFixed(5); // HELP: does not work on x - PI ???
-    }
-    
-    // cele-moo-shun
-    moo.play();
     
     // clear input for next round
     resetVariables();
